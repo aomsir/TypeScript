f06feb11eb2422c219b5725865edf88ffa5acebe
aomsir <info@say521.cn>
2022-12-11 20:29:13 +0800

    ts类型初探

--- day01/No1/01-类型注解的使用.ts
+++ day01/No1/01-类型注解的使用.ts
@@ -1,8 +1,18 @@
 let age :number = 20
 let names :string = 'Aomsir'
 
-
+let isDog :boolean = true
+let un :undefined = undefined
+let null1 :null = null
+let sym :symbol = Symbol()
 
 console.log(age);
 console.log(names);
+console.log(isDog);
+console.log(un);
+console.log(null1);
+console.log(sym);
+
+
+
 
