97e1210a77517b6fc5bdd7d3db2b6ae61fea35cc
aomsir <info@say521.cn>
2022-12-12 15:04:13 +0800

    数组类型

--- day01/No1/01-类型注解的使用.ts
+++ day01/No1/01-类型注解的使用.ts
@@ -1,18 +1,20 @@
-let age :number = 20
-let names :string = 'Aomsir'
+{
+    let age :number = 20
+    let name :string = 'Aomsir'
 
-let isDog :boolean = true
-let un :undefined = undefined
-let null1 :null = null
-let sym :symbol = Symbol()
+    let isDog :boolean = true
+    let un :undefined = undefined
+    let null1 :null = null
+    let sym :symbol = Symbol()
 
-console.log(age);
-console.log(names);
-console.log(isDog);
-console.log(un);
-console.log(null1);
-console.log(sym);
+    console.log(age);
+    console.log(name);
+    console.log(isDog);
+    console.log(un);
+    console.log(null1);
+    console.log(sym);
 
+}
 
 
 
---
+++ day01/No1/02-数组类型.ts
@@ -0,0 +1,14 @@
+{
+    // 数组定义方式一
+    const numbers :number[] = [1,2,3]
+    const names :string[] = ['Aomsir', 'Jewix']
+    console.log(numbers);
+    console.log(names);
+    
+
+    // 数组定义方式二
+    let strings :Array<string> = ['Aomsir', 'Jewix']
+    console.log(strings);
+    
+    
+}
